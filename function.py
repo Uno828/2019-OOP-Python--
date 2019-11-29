ed82e34befc51283b60d5eda71fb29fe47d93f46
김태웅 <twk9975@sasa.hs.kr>
2019-11-30 01:08:10 +0900

    Merge branch 'master' of C:\Users\김태웅\PycharmProjects\2019-OOP-Python- with conflicts.

--- function.py
+++ function.py
@@ -15,3 +15,23 @@ def num_check(event):
 
 def card_show(card_num): #화면에 선택된 카드를 띄워주는 함수
     pass
+
+def ranking(people, n):
+    for i in range(n):
+        people[i].calculate()
+
+    people.sort(key=lambda rank: rank.score, reverse=True)
+
+    player1 = pygame.image.load("p1")
+    player2 = pygame.image.load("p2")
+    player3 = pygame.image.load("p3")
+    player4 = pygame.image.load("p4")
+    player5 = pygame.image.load("p5")
+    player6 = pygame.image.load("p6")
+    player7 = pygame.image.load("p7")
+    player_empty = pygame.image.load("p_emt")
+
+    """
+    for i in range(len(people)):
+        print("{0}등 - {1}p : {2}points ".format(i + 1, people[i].player, people[i].score))
+    """
