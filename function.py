b38e3d84812a26177811cbd19fe4ecbb07f59dc8
zeuszues <wodnjack@naver.cpm>
2019-11-30 01:21:55 +0900

    Merge branch 'master' of /Users/jaiwoo/PycharmProjects/2019-OOP-Python-- with conflicts.

--- function.py
+++ function.py
@@ -13,5 +13,16 @@ def num_check(event):
 
     return None
 
-def card_show(card_num): #화면에 선택된 카드를 띄워주는 함수
-    pass
+def card_show(card_num,screen): #화면에 선택된 카드를 띄워주는 함수
+    card = pygame.image.load("image/"+str(-card_num)+".png")
+    screen.blit(card, (784, 58))
+    pygame.display.flip()
+
+def auction(event):
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            return 0
+        elif event.key == pygame.K_RIGHT:
+            return 1
+    return None
+
--- main_eunho.py
+++ main_eunho.py
@@ -3,18 +3,18 @@ import time
 import pygame
 from function import *
 
-
 class human:
     def __init__(self, player, coin):
-        self.player = player + 1  # player 몇번째 사람인가? ->
-        self.coin = coin  # 칩의 개수
-        self.card = []  # 보유한 카드
-        self.score = 0  # 최종 스코어
+        self.player = player + 1        # player 몇번째 사람인가? ->
+        self.coin = coin        # 칩의 개수
+        self.card = []          # 보유한 카드
+        self.score = 0          # 최종 스코어
+        
 
-    def calculate(self):  # 스코어 계산 과정
-        self.card.sort(reverse=True)  # 크기 역순으로 정렬 ex) 8 7 6 5
-        self.score = self.coin  # 칩 개수부터 스코어 계산
-        back_num = 0  # 연속한 수 체크하는 tmp
+    def calculate(self):        # 스코어 계산 과정
+        self.card.sort(reverse=True)    # 크기 역순으로 정렬 ex) 8 7 6 5
+        self.score = self.coin          # 칩 개수부터 스코어 계산
+        back_num = 0                    # 연속한 수 체크하는 tmp
 
         for i in range(len(self.card)):
             if self.card[i] == back_num - 1:
@@ -23,14 +23,13 @@ class human:
                 self.score += self.card[i]
             back_num = self.card[i]
 
-
 if __name__ == '__main__':
 
     pygame.init()
     chip = pygame.image.load("image/chip.png")
     board = pygame.image.load("image/board.png")
     start_screen = pygame.image.load("image/start.png")
-    main_screen = pygame.image.load("image/main.png")
+    main_screen = pygame.image.load("image/ing.png")
     run = True
     width, height = 960, 720
     screen = pygame.display.set_mode((width, height))
@@ -40,40 +39,38 @@ if __name__ == '__main__':
     while run:
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type==pygame.QUIT:
                 run = False
 
         screen.blit(start_screen, (0, 0))
         pygame.display.flip()
 
-        people = []  # 본격적인 사람의 수
+        people = [] # 본격적인 사람의 수
 
-        if n is not None:
+        if n!= None:
             n = n
-
         else:
             n = num_check(event)
 
         if n is not None:
             n = int(n)
-            print(n)
-            screen.blit(main_screen, (0, 0))
+            screen.blit(main_screen,(0,0))
             pygame.display.flip()
             for i in range(n):  # n명 가정, 0~n-1
-                people.append(human(i, 9))
+                people.append(human(i, 20))
 
-            deck = []  # 턴에 결정될 카드
-            for i in range(-35, -2):  # 현재 카드가 -3 ~ -35까지 모두 append
+            deck = []   # 턴에 결정될 카드
+            for i in range(-35, -2):    # 현재 카드가 -3 ~ -35까지 모두 append
                 deck.append(i)
 
             turn = 0  # 0 ~ n-1     # 몇번째 플레이어의 순서인가
 
-            while len(deck) and False:  # 카드가 더 이상 남아있지 않을때까지
-                num = random.choice(deck)  # 현재 카드 결정
-                card_show(num)  # 결정된 현재 숫자를 화면에 띄워 주는 함수
+            while len(deck):  # 카드가 더 이상 남아있지 않을때까지
+                num = random.choice(deck) # 현재 카드 결정
+                card_show(num,screen)#결정된 현재 숫자를 화면에 띄워 주는 함수
 
-                chk = 1  # 1은 패스, 0은 낙찰
-                stacked_coin = 0  # 현재 카드에 쌓인 칩의 개수
+                chk = -1  # 1은 패스, 0은 낙찰
+                stacked_coin = 0    # 현재 카드에 쌓인 칩의 개수
                 turn -= 1
                 while chk:  # 낙찰받는 사람 결정
                     turn += 1  # 다음 사람으로 넘기기
@@ -82,34 +79,16 @@ if __name__ == '__main__':
                         print("%dp님, 당신은 칩이 없습니다." % (turn + 1), end=' ')
                         break
                     while not (chk == 0 or chk == 1):
-                        pass  # 왼쪽:0, 매수|오른쪽:1, 패스
+                        for event in pygame.event.get():
+                            chk = auction(event)
+                            print(chk)
 
                     if chk:
                         stacked_coin += 1  # 패스하면, 칩 개수 올리고
                         people[turn].coin -= 1  # 가진 칩 줄이기
+                        chk=-1
                 print("%dp님, 낙찰되었습니다." % (turn + 1))  # 낙찰된 상황
                 people[turn].coin += stacked_coin  # 칩 개수만큼 올렺고
                 people[turn].card.append(num)  # 갖고 있는 카드에 추가
                 deck.remove(num)  # 방금 카드 지워버리기
-
-            # game logic rank
-            # for i in range(n):
-            #     people[i].calculate()
-            #
-            # print("-----CARD-----")
-            #
-            # for i in range(n):
-            #     print("%dp : " % people[i].player, end='')
-            #     for j in range(len(people[i].card)):
-            #         print("%d" % people[i].card[j], end=' ')
-            #     print("\nChip: %d" % people[i].coin)
-            #     print()
-            #
-            # people.sort(key=lambda rank: rank.score, reverse=True)
-            #
-            # print("-----RANKING-----")
-            #
-            # for i in range(len(people)):
-            #     print("{0}등 - {1}p : {2}points ".format(i + 1, people[i].player, people[i].score))
-
     pygame.quit()
