992a2c67dfae5e77605ff589a4992531789f8892
zeuszues <wodnjack@naver.cpm>
2019-11-30 04:20:53 +0900

    GUI 제작 진행 중, 이 버전의 python file로 각자 맡은 부분 구현 시작

--- func.py
+++ func.py
@@ -37,6 +37,7 @@ def image_show(image, x, y, screen):
 
 
 def text_show(txt, size, x, y, screen):
+    locus=[(822.2, 386.25),(857.44, 495.25),(859.44, 610.25),(150, 150)]
     fontObj = pygame.font.Font('myfont.ttf', size)  # 현재 디렉토리로부터 myfont.ttf 폰트 파일을 로딩한다. 텍스트 크기를 size로 한다
     textSurfaceObj = fontObj.render(txt, True,
                                     Black)  # 텍스트 객체를 생성한다. 첫번째 파라미터는 텍스트 내용, 두번째는 Anti-aliasing 사용 여부, 세번째는 텍스트 컬러를
@@ -82,35 +83,46 @@ def ranking(people, n, screen):
 
     people.sort(key=lambda rank: rank.score, reverse=True)
 
-    first = pygame.image.load("p" + str(people[0].player) + "- 큰 결과.png")
-    second = pygame.image.load("p" + str(people[1].player) + "- 큰 결과.png")
-    third = pygame.image.load("p" + str(people[2].player) + "- 큰 결과.png")
-    fourth = pygame.image.load("p" + str(people[3].player) + "- 작은 결과.png")
+    lank=[]
+    for i in range(n):
+        lank.append(pygame.image.load("p"+str(people[i].player)+"- 큰 결과.png"))
+    for i in range(7-n):
+        lank.append(pygame.image.load("p_emt- 작은 결과.png"))
 
-    if n == 4:
-        fifth = pygame.image.load("p_emt- 작은 결과.png")
-        sixth = pygame.image.load("p_emt- 작은 결과.png")
-        seventh = pygame.image.load("p_emt- 작은 결과.png")
+    # first = pygame.image.load("p" + str(people[0].player) + "- 큰 결과.png")
+    # second = pygame.image.load("p" + str(people[1].player) + "- 큰 결과.png")
+    # third = pygame.image.load("p" + str(people[2].player) + "- 큰 결과.png")
+    # fourth = pygame.image.load("p" + str(people[3].player) + "- 작은 결과.png")
+    #
+    # if n == 4:
+    #     fifth = pygame.image.load("p_emt- 작은 결과.png")
+    #     sixth = pygame.image.load("p_emt- 작은 결과.png")
+    #     seventh = pygame.image.load("p_emt- 작은 결과.png")
+    #
+    # if n == 5:
+    #     fifth = pygame.image.load("p" + str(people[4].player) + "- 작은 결과.png")
+    #     sixth = ("p_emt- 작은 결과.png")
+    #     seventh = ("p_emt- 작은 결과.png")
+    #
+    # if n == 6:
+    #     fifth = pygame.image.load("p" + str(people[4].player) + "- 작은 결과.png")
+    #     sixth = pygame.image.load("p" + str(people[5].player) + "- 작은 결과.png")
+    #     seventh = ("p_emt- 작은 결과.png")
+    #
+    # if n == 7:
+    #     fifth = pygame.image.load("p" + str(people[4].player) + "- 작은 결과.png")
+    #     sixth = pygame.image.load("p" + str(people[5].player) + "- 작은 결과.png")
+    #     seventh = pygame.image.load("p" + str(people[6].player) + "- 작은 결과.png")
 
-    if n == 5:
-        fifth = pygame.image.load("p" + str(people[4].player) + "- 작은 결과.png")
-        sixth = ("p_emt- 작은 결과.png")
-        seventh = ("p_emt- 작은 결과.png")
+    locx=[403.024, 202.524, 603.524, 179.978, 336.978, 503.978, 659.978]
+    locy=[256.374, 256.374, 256.374, 394.752, 394.752, 394.752, 394.752]
+    for i in range(7):
+        image_show(lank[i],locx[i],locy[i],screen)
 
-    if n == 6:
-        fifth = pygame.image.load("p" + str(people[4].player) + "- 작은 결과.png")
-        sixth = pygame.image.load("p" + str(people[5].player) + "- 작은 결과.png")
-        seventh = ("p_emt- 작은 결과.png")
-
-    if n == 7:
-        fifth = pygame.image.load("p" + str(people[4].player) + "- 작은 결과.png")
-        sixth = pygame.image.load("p" + str(people[5].player) + "- 작은 결과.png")
-        seventh = pygame.image.load("p" + str(people[6].player) + "- 작은 결과.png")
-
-    image_show(first, 403.024, 256.374, screen)
-    image_show(second, 202.524, 256.374, screen)
-    image_show(third, 603.524, 256.374, screen)
-    image_show(fourth, 179.978, 394.752, screen)
-    image_show(fifth, 336.978, 394.752, screen)
-    image_show(sixth, 503.978, 394.752, screen)
-    image_show(seventh, 659.978, 394.752, screen)
+    # image_show(first, 403.024, 256.374, screen)
+    # image_show(second, 202.524, 256.374, screen)
+    # image_show(third, 603.524, 256.374, screen)
+    # image_show(fourth, 179.978, 394.752, screen)
+    # image_show(fifth, 336.978, 394.752, screen)
+    # image_show(sixth, 503.978, 394.752, screen)
+    # image_show(seventh, 659.978, 394.752, screen)
